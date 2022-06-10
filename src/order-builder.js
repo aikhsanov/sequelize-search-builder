0cc0b0f70202414c61cd3405e6c62e43ae305e2e
aikhsanov <71754926+aikhsanov@users.noreply.github.com>
2022-06-10 09:51:10 +0300

    Update order-builder.js

--- src/order-builder.js
+++ src/order-builder.js
@@ -1,12 +1,16 @@
 const BuilderAbstract = require('./builder-abstract');
 
+
+/* собирает массив для сортировки из распарсенного через qs объекта */
 class OrderBuilder extends BuilderAbstract {
   getQuery() {
     const { request } = this;
     const query = [];
 
     Object.keys(request).forEach((key) => {
+      /* создаём массив из ключа */
       const value = key.split('.');
+      /* в массив добавляется значение */
       value.push(request[key]);
       query.push(value);
     });
